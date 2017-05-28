7abc316b6ebcc7922ba39473fe353ce3c7f34980
WDN2010 <2010WDN@gmail.com>
2017-05-28 19:44:54 +0300

    Update StringsTask1.java

--- LearnJava/src/StringsTask1.java
+++ LearnJava/src/StringsTask1.java
@@ -3,38 +3,38 @@ public class StringsTask1 {
 
 	public static void main(String[] args) {
 		String str=new String ("Жила-была коза с козлятами. Уходила коза в лес есть траву шелковую, пить воду студеную. Как только уйдет - козлятки запрут избушку и сами никуда не выходят. Воротится коза, постучится в дверь и запоет: - Козлятки, ребятушки! Отопритеся, отворитеся! Ваша мать пришла - молока принесла; Бежит молоко по вымечку, Из вымечку по копытеччку, Из копытеччку во сыру землю! Козлятки отопрут дверь и впустят мать. Она их покормит, напоит и опять уйдет в лес, козлятки запрутся крепко-накрепко. Волк подслушал, как поет коза. Вот раз коза ушла, волк побежал к избушке и закричал толстым голоском: - Вы, детушки! Вы козлятки! Отопритеся, Отворитеся! Ваша мать пришла, Молока принесла. Полны копытца водицы!");
-		str=str.toLowerCase(); /*низкий регистр*/
+		str=str.toLowerCase(); 
 		String tmp=new String();
-			tmp = str.replace(".", ""); /*удаление "."*/
+			tmp = str.replace(".", ""); 
 			str = tmp;
-			tmp = str.replace(",", ""); /*удаление ","*/
+			tmp = str.replace(",", ""); 
 			str = tmp;
-			tmp = str.replace(":", ""); /*удаление ":"*/
+			tmp = str.replace(":", ""); 
 			str = tmp;
-			tmp = str.replace("-", " "); /*удаление "-"*/
+			tmp = str.replace("-", " "); 
 			str = tmp;
-			tmp = str.replace("!", ""); /*удаление "!"*/
+			tmp = str.replace("!", "");
 			str = tmp;
-			tmp = str.replace(";", ""); /*удаление ";"*/
+			tmp = str.replace(";", ""); 
 			str = tmp;
-			tmp = str.replace("  ", " "); /*удаление лишних пробелов*/
+			tmp = str.replace("  ", " "); 
 			str = tmp;
 			System.out.println(str);
-		/*Создание массива, где каждый эл-т - отельное слово*/	
+			
 		String[] strParts;
 		strParts = str.split(" ");
 		for (int n=0; n<strParts.length;n++){
-			strParts[n].trim(); /*Удаление ведущих и замыкающих пробелов в словах*/
+			strParts[n].trim(); 
 		}
 		
 		for (int i=0; i<strParts.length;i++){
 			int sum=0;
 			if (strParts[i]!=" "){
-				System.out.print(strParts[i]+":"); /*Выводит слово*/
+				System.out.print(strParts[i]+":"); 
 				int k=0;
-				int[] number=new int[104]; /*Массив для записи местоположений этого слова*/
+				int[] number=new int[104]; 
 				for(int j=0;j<strParts.length;j++){
-					if (strParts[i].equals(strParts[j]))/*поиск этого слова*/
+					if (strParts[i].equals(strParts[j]))
 					{ 
 						sum=sum+1; 
 						number[k]=j;
@@ -44,7 +44,7 @@ public class StringsTask1 {
 				
 				System.out.print(sum);
 				System.out.println();
-				for (k=0;k<104;k ++) /*Заменяет найденные слова на пробелы*/
+				for (k=0;k<104;k ++) 
 				{
 					strParts[number[k]]=" ";
 				} 
